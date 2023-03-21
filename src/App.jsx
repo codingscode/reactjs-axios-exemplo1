5a3ced2a7f706ac72771ae251dca80e59e3b312f
codingscode <heronnestg@hotmail.com>
2023-03-21 10:58:48 -0300

    parte 6

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ function App() {
 
    return (
       <div className='App' >
+         <Navbar />
          <div className='container' >
-            <Navbar />
             <Outlet />
          </div>
       </div>

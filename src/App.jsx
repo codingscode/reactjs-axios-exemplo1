806f9dfcc9e5271718163c7a3cd27a59708be015
codingscode <heronnestg@hotmail.com>
2023-03-20 22:42:05 -0300

    parte 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 
+import { Outlet } from 'react-router-dom'
 import './App.css'
+
 
 
 
 function App() {
 
-
    return (
-      <div >
-         <h1>React Axios</h1>
+      <div className='App' >
+         <div className='container' >
+            <Outlet />
+         </div>
       </div>
    )
 }

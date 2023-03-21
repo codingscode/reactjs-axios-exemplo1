98af9952ae248c99c1b622b9a05e43f98c7bd174
codingscode <heronnestg@hotmail.com>
2023-03-21 15:24:11 -0300

    parte 18

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,12 +1,24 @@
 
+import blogFetch from '../axios/config'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import './NewPost.css'
 
 
 const NewPost = () => {
+   const navigate = useNavigate()
+   
+   const [title, setTitle] = useState()
+   const [body, setBody] = useState()
+   
+   const createPost = () => {
+   
+   }
+
    return (
       <div className='new-post' >
          <h2>Inserir novo Post:</h2>
-         <form>
+         <form onSubmit={() => createPost()} >
             <div className='form-control' >
                <label htmlFor='title' >Título</label>
                <input type='text' id='title' name='title' placeholder='Digite o título' />

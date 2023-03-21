2101d05eb13a620cc6fc2ed3372521ef72858125
codingscode <heronnestg@hotmail.com>
2023-03-21 12:22:17 -0300

    parte 15

--- src/axios/config.js
+++ src/axios/config.js
@@ -1,12 +1,14 @@
 
-
 import axios from 'axios'
 
 
 const blogFetch = axios.create({
-   baseURL: 'https://jsonplaceholder.typicode.com'
+   baseURL: 'https://jsonplaceholder.typicode.com',
+   headers: {
+      'Content-Type': 'application/json',
+      algumacoisa: 'teste'
+   }
 
 })
 
 export default blogFetch
-
